63ff0efb55cd853a454984f4a37814e32ee6b013
Clemens Lieb <Vogel612@users.noreply.github.com>
2017-05-22 14:26:10 +0200

    Update contributor-breakdown userscript
    Document-Structure was changed by SE, so we need to adjust the selectors

--- documentation-contributor-breakdown.user.js
+++ documentation-contributor-breakdown.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Documentation contributor links
 // @namespace    http://github.com/Vogel612/mini-se-userscripts/documentation-contributor-breakdown
-// @version      0.1
+// @version      0.2
 // @description  Add links to the contributor breakdown for topics and examples on stackoverflow documentation.
 // @author       Vogel612
 // @include      /https?:\/\/stackoverflow\.com\/documentation\/.*
@@ -13,18 +13,18 @@
     // these are in here for debugging purposes
     document.addLinks = addLinks;
     document.addContributorLink = addContributorLink;
+    addLinks();
 })();
 // because the page is modified on SE side after loading. 2 secs should be enough to wait
-setTimeout(addLinks, 2000);
 
 function addLinks() {
     'use strict';
     let topicLink = document.querySelectorAll("a.doc-topic-link")[0];
-    let topicMenuContainer = document.querySelector("div.docs-menu.topic-menu");
+    let topicMenuContainer = document.querySelector(".topic-section div.docs-menu .menu-for-show");
     addContributorLink("topic", topicLink.href.split('/')[5], topicMenuContainer);
 
     let exampleLinks = document.querySelectorAll(".example-link a.doc-example-link");
-    let exampleMenus = document.querySelectorAll(".example-link div.example-menu.docs-menu");
+    let exampleMenus = document.querySelectorAll("div.example-menu.docs-menu .menu-for-show");
     for (var i = 0; i < exampleLinks.length; i++) {
         let currentLink = exampleLinks[i];
         let currentMenuContainer = exampleMenus[i];
@@ -34,10 +34,6 @@ function addLinks() {
 
 function addContributorLink(section, linkId, menu) {
     'use strict';
-    let href = `/documentation/contributors/${section}/${linkId}`;
-    let elem = document.createElement("A");
-    elem.href = href;
-    elem.innerHTML = "CB";
-    elem.title = "Contributor Breakdown";
-    menu.insertBefore(elem, menu.children[0]);
+    menu.insertAdjacentHTML('beforeend',
+           `<a href="/documentation/contributors/${section}/${linkId}" target="cb_${linkId}" title="Contributor Breakdown">CB</a>`);
 }
